f63fcd3a2e69ea4f4a44ce7246f748e514925d9c
Krzysztof Łakwa <krzysztof.lakwa@britenet.com.pl>
2018-12-05 15:31:05 +0100

    Add CommentBox content

---
+++ src/components/CommentBox.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class CommentBox extends Component {
+  state = {
+    comment: ''
+  }
+
+  handleChange = e => {
+    this.setState({ comment: e.target.value });
+  }
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.setState({ comment: '' });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h4>Add comment</h4>
+        <textarea
+          onChange={this.handleChange}
+          value={this.state.comment}
+        />
+        <div>
+          <button>Submit comment</button>
+        </div>
+      </form>
+    );
+  }
+};
+
+export default CommentBox;
