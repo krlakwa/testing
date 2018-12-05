9c23d40a98ff1f1b698eec5103f388f400e567ca
Krzysztof Łakwa <krzysztof.lakwa@britenet.com.pl>
2018-12-05 15:30:31 +0100

    Add app component unit tests

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
+
 export default () => {
   return (
     <div>
-      App component
+      <CommentBox />
+      <CommentList />
     </div>
   );
 };
